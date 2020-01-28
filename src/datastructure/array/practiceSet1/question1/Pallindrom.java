3a3b592bf63d37fab6c3c7f06a448ed023825f09
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-28 19:11:18 +0530

    create methods in Palindrome class

--- src/datastructure/array/practiceSet1/question1/Pallindrom.java
+++ src/datastructure/array/practiceSet1/question1/Pallindrom.java
@@ -1,5 +1,30 @@
 package datastructure.array.practiceSet1.question1;
 
 class Palindrom {
+    private String string = "";
 
+    public Palindrom(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public Boolean Pallindrome(String string, int l) {
+        String reverse = "";
+        for (int i = l - 1; i >= 0; i--) {
+            char ch = string.charAt(i);
+            reverse = reverse + ch;
+        }
+        if (reverse.equals(string)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
