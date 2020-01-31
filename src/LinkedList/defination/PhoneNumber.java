e72bdcd87bdcad65186aeda75e9b6fac0e93191a
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-31 16:00:44 +0530

    getter setter for all the fields in Phone Number Class

--- src/LinkedList/defination/PhoneNumber.java
+++ src/LinkedList/defination/PhoneNumber.java
@@ -5,4 +5,43 @@ public class PhoneNumber {
     private int stdCode;
     private long number;
     private boolean isSameCountry;
+
+    public PhoneNumber(int countryCode, int stdCode, long number, boolean isSameCountry) {
+        this.countryCode = countryCode;
+        this.stdCode = stdCode;
+        this.number = number;
+        this.isSameCountry = isSameCountry;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(int countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getStdCode() {
+        return stdCode;
+    }
+
+    public void setStdCode(int stdCode) {
+        this.stdCode = stdCode;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public boolean isSameCountry() {
+        return isSameCountry;
+    }
+
+    public void setSameCountry(boolean sameCountry) {
+        isSameCountry = sameCountry;
+    }
 }
