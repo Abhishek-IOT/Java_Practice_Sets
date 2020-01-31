8fefd1526bca06bd32fea46305e5b026b680ac76
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-31 16:01:47 +0530

    create StringBuffer for fields in Contact class

--- src/LinkedList/defination/Contact.java
+++ src/LinkedList/defination/Contact.java
@@ -8,6 +8,16 @@ public class Contact {
     private List<PhoneNumber> numbers;
     private String email;
 
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Contact{");
+        sb.append("FirstName='").append(FirstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", numbers=").append(numbers);
+        sb.append(", email='").append(email).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
     public Contact(String firstName, String lastName, List<PhoneNumber> numbers, String email) {
         FirstName = firstName;
         this.lastName = lastName;
