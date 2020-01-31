fbe38b957e8e86ed6916324ac0403d2ddeff7fa9
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-31 15:55:56 +0530

    create class contact

---
+++ src/LinkedList/defination/Contact.java
@@ -0,0 +1,11 @@
+package LinkedList.defination;
+
+import java.util.List;
+
+public class Contact {
+    private String FirstName;
+    private String lastName;
+    private List<PhoneNumber> numbers;
+    private String email;
+
+}
---
+++ src/LinkedList/defination/PhoneNumber.java
@@ -0,0 +1,8 @@
+package LinkedList.defination;
+
+public class PhoneNumber {
+    private int CountryCode;
+    private int stdCode;
+    private long number;
+    private boolean
+}
