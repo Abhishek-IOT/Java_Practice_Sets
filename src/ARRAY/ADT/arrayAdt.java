465913f1713f9fdf061f53ec5344e0e74f344866
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-02 09:55:20 +0530

    created abstract methods for interface arrayAdt

---
+++ src/ARRAY/ADT/arrayAdt.java
@@ -0,0 +1,16 @@
+package ARRAY.ADT;
+
+public interface arrayAdt {
+    public boolean arrayCreated();
+
+    public int retrieveIndex();
+
+    public int storeData();
+
+    public int sort();
+
+    public int countTheElements();
+
+    public int removeAnElement();
+
+}
