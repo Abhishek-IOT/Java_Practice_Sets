d6ba4f99a5be459d7809146f08cf7493ce9bfbf4
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-02 09:56:55 +0530

    created Defination class and now going to implement the arrayAdt methods in it

---
+++ src/ARRAY/DEFINATION/Defination.java
@@ -0,0 +1,29 @@
+package ARRAY.DEFINATION;
+
+import ARRAY.ADT.arrayAdt;
+
+public class Defination implements arrayAdt {
+    public boolean arrayCreated() {
+        return false;
+    }
+
+    public int retrieveIndex() {
+        return 0;
+    }
+
+    public int storeData() {
+        return 0;
+    }
+
+    public int sort() {
+        return 0;
+    }
+
+    public int countTheElements() {
+        return 0;
+    }
+
+    public int removeAnElement() {
+        return 0;
+    }
+}
