90d9f91be5fc980c499dd2f49a06c8d5090e6cd6
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-03 19:02:55 +0530

    created the main class for the array

--- src/ARRAY/DEFINATION/Defination.java
+++ src/ARRAY/DEFINATION/Defination.java
@@ -25,6 +25,7 @@ public class Defination implements arrayAdt {
     public int retrieveIndex(int size, int element) {
         int arr[] = new int[size];
         int temp = 0;
+
         for (int i = 0; i < size; i++) {
             if (arr[i] == element)
                 temp = i;
---
+++ src/ARRAY/Main/Main.java
@@ -0,0 +1,14 @@
+package ARRAY.Main;
+
+import ARRAY.DEFINATION.Defination;
+
+public class Main {
+    public static void main(String[] args) {
+        Defination defination = new Defination();
+        defination.arrayCreated(10);
+        defination.countTheElements(10);
+        defination.removeAnElement(10, 5);
+        defination.sort(10);
+        defination.storeData(10);
+    }
+}
