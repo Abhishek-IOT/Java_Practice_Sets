8c303185ec38771c52cbf01dd647590dc262681b
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-09 09:35:16 +0530

    created main class

---
+++ src/Udemy/Main.java
@@ -0,0 +1,9 @@
+package Udemy;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(4, 5);
+
+        Cuboid cuboid = new Cuboid(4, 5, 6);
+    }
+}
