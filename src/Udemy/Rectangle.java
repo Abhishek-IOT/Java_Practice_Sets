380e528efc8043d9b0943c9201185a9a36c33be8
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-09 09:14:43 +0530

    checking for length and width values

--- src/Udemy/Rectangle.java
+++ src/Udemy/Rectangle.java
@@ -5,7 +5,11 @@ public class Rectangle {
     private double width;
 
     public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
+        if (width < 0) {
+            width = 0;
+        }
+        if (length < 0) {
+            length = 0;
+        }
     }
 }
