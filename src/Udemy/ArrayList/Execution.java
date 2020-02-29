7d25b644c70757c9144a329fbaa9802f84cd48a8
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-29 07:44:58 +0530

    finally done with the main class

--- src/Udemy/ArrayList/Execution.java
+++ src/Udemy/ArrayList/Execution.java
@@ -5,6 +5,53 @@ import java.util.Scanner;
 public class Execution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        boolean yo = true;
+        arrayList arr = new arrayList();
+        System.out.println("Welcome to Abhishek List\n" +
+                "Press 1 for adding the data\n" +
+                "Press 2 for deleting the data\n" +
+                "Press 3 for viewing all the data\n" +
+                "Press 4 for modifying the data\n" +
+                "Press 5 for searching the data\n" +
+                "Press 6 for exiting from the menu");
+        while (yo == true) {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("You have selected for adding the data\n" +
+                            "Give the data to be added");
+                    String data = scanner.next();
+                    arr.addData(data);
+                    break;
+                case 2:
+                    System.out.println("You have enter to remove the data\n" +
+                            "Please tell the location from where index has to be removed");
+                    int index = scanner.nextInt();
+                    arr.removeData(index);
+                    break;
+                case 3:
+                    System.out.println("You have entered for viewing all the data");
+                    arr.viewAllData();
+                    break;
+                case 4:
+                    System.out.println("You have entered for modifying the data\n" +
+                            "Press  enter the data to be modified and at which index to be mdified");
+                    int r = scanner.nextInt();
+                    String info = scanner.next();
+                    arr.modifyData(info, r);
+                    break;
+                case 5:
+                    System.out.println("You have entered to search the data");
+                    String information = scanner.next();
+                    arr.searchingData(information);
+                    break;
+                case 6:
+                    System.out.println("YOu have entered for ecxiting from the menu thank you");
+                    yo = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice found");
+            }
+        }
     }
 }
