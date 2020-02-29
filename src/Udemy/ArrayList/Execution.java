136aae783fdb028ce31376d529e6456dc24475c1
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-29 07:14:18 +0530

    created the main class and imported the scanner class

---
+++ src/Udemy/ArrayList/Execution.java
@@ -0,0 +1,10 @@
+package Udemy.ArrayList;
+
+import java.util.Scanner;
+
+public class Execution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
